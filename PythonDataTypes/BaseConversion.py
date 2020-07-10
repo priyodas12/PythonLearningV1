e7a6ec2f93f2f30c7ea9c487fc1d6e226d58cf97
Priyobrato Das <priyodas12@gmail.com>
2020-07-11 00:35:15 +0530

    complex

--- PythonDataTypes/BaseConversion.py
+++ PythonDataTypes/BaseConversion.py
@@ -7,12 +7,12 @@ print(hex(a))
 # octal to bool,oct,hex
 b=0o777
 print(bin(b))
-print(oct(b))
+print(int(b))
 print(hex(b))
 
-# hex to bool,oct,hex
+# hex to bool,oct,int
 c=0xacf47
 print(bin(c))
 print(oct(c))
-print(hex(c))
+print(int(c))
 
---
+++ PythonDataTypes/FloatType.py
@@ -0,0 +1,4 @@
+f1=1.3e4
+print(f1)
+f2=1.3e-4
+print(f2)
