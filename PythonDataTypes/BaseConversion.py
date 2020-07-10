6c70e3ea4a0974e66628baa96c17f7e2295c6467
Priyobrato Das <priyodas12@gmail.com>
2020-07-11 00:28:39 +0530

    conversion

---
+++ PythonDataTypes/BaseConversion.py
@@ -0,0 +1,18 @@
+# int to bool,oct,hex
+a=101
+print(bin(a))
+print(oct(a))
+print(hex(a))
+
+# octal to bool,oct,hex
+b=0o777
+print(bin(b))
+print(oct(b))
+print(hex(b))
+
+# hex to bool,oct,hex
+c=0xacf47
+print(bin(c))
+print(oct(c))
+print(hex(c))
+
