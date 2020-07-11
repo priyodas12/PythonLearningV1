acd33e82884715552847e1a5c72898ecd8f88fd6
Priyobrato Das <priyodas12@gmail.com>
2020-07-11 11:56:00 +0530

    string ops

--- PythonDataTypes/StringOperator.py
+++ PythonDataTypes/StringOperator.py
@@ -2,3 +2,7 @@ s='abcdefghij'
 # 1st one is inclusive,2nd one exclusive
 print(s[2:5])
 print(s[-3:])
+# positive indexing(left to right)
+print(s[0])
+# negative indexing(right to left)
+print(s[-1])
