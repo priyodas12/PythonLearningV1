5816f8502c43117d762ac80f23cd7ec18c5b064a
Priyobrato Das <priyodas12@gmail.com>
2020-07-11 19:44:31 +0530

    set

--- PythonDataTypes/SetType.py
+++ PythonDataTypes/SetType.py
@@ -5,9 +5,12 @@ set:
 2. no insertion order maintained
 3. duplicates not allowed
 4. index,slicing terminology not applicable for set
+5. mutable,growable
 """
 s={10,20,30,40,10}
+s.add(123)
+s.remove(40)
 
 for x in s:print(x) # duplicate will not be added.
-print(s[0]) # TypeError: 'set' object is not subscriptable
+# print(s[0]) # TypeError: 'set' object is not subscriptable
 
