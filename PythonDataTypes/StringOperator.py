fb0f8b5e0a7976781f76212bc7138bee2ad1e043
Priyobrato Das <priyodas12@gmail.com>
2020-07-11 12:10:50 +0530

    string ops

--- PythonDataTypes/StringOperator.py
+++ PythonDataTypes/StringOperator.py
@@ -1,7 +1,5 @@
 s='abcdefghij'
-# 1st one is inclusive,2nd one exclusive
-print(s[2:5])
-print(s[1:-1])
+
 # positive indexing(left to right)
 print(s[0])
 # negative indexing(right to left)
@@ -13,4 +11,11 @@ print(s[-1])
 
 # capture rest
 print(s[4:])
+
+# capture from first up to
+print(s[:4])
+
+# 1st one is inclusive,2nd one exclusive
+print(s[2:5])
+print(s[1:-1])
 
