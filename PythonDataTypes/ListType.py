f2d7925c02ce520745ceb780d14ff9159ccc1c08
Priyobrato Das <priyodas12@gmail.com>
2020-07-11 19:09:07 +0530

    bytearray

---
+++ PythonDataTypes/ListType.py
@@ -0,0 +1,20 @@
+"""
+1. insertion order maintained.
+2. duplicates are allowed.
+3. list convention []
+4. heterogeneous object si allowed.
+"""
+l=[1,2,3,4,5,6,'abc',True,1.0,1,None]
+print(len(l))
+
+# alter elements
+l[10]=10
+
+# type of
+print(type(l))
+
+# print all elements
+for x in l:print(x)
+
+# slice operation
+print(l[1:4])
