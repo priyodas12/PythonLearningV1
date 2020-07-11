a598c08c431d22a928db8e2ef44ca1c44edf10b2
Priyobrato Das <priyodas12@gmail.com>
2020-07-11 12:06:49 +0530

    string ops

--- PythonDataTypes/StringOperator.py
+++ PythonDataTypes/StringOperator.py
@@ -1,8 +1,13 @@
 s='abcdefghij'
 # 1st one is inclusive,2nd one exclusive
 print(s[2:5])
-print(s[-3:])
+print(s[1:-1])
 # positive indexing(left to right)
 print(s[0])
 # negative indexing(right to left)
 print(s[-1])
+
+# out of range
+print(s[10])
+# IndexError: string index out of range
+
